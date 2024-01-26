e1d433ac350b5cbda94ea4ab29d3a85308c22b05
deveshbedmutha1406 <deveshbedmutha500@gmail.com>
2024-01-26 22:37:45 +0530

    ADDED CORS CONFIG

--- OPS/settings.py
+++ OPS/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'drf_yasg',
     'core',
     'rest_framework',
@@ -51,7 +52,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
+]
+
 
 ROOT_URLCONF = 'OPS.urls'
 
